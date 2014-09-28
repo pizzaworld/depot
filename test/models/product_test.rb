bfbe4c72319009f79c440fa9b081a5d01679e772
Sara Cavic <saracavic@gmail.com>
2014-09-28 14:45:59 -0400

    Validation of unique product titles

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -43,4 +43,13 @@ test "image url" do
     assert new_product(name).invalid?, "#{name} shouldn't be valid"
   end
  end
+test "product is not valid without a unique title - i18n" do
+  product = Product.new(title: products(:ruby).title,
+                        description: "yyy",
+                        price: 1,
+                        image_url: "fred.gif")
+  assert product.invalid?
+  assert_equal [I18n.translate('errors.messages.taken')],
+    product.errors[:title]
+ end
 end
