603577a15b8a0e55ea86ee6c1252645b0240ce33
Sara Cavic <saracavic@gmail.com>
2014-10-11 22:32:35 -0400

    Configure environments for sending customer emails

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,17 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-
 # Initialize the Rails application.
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "localhost:3000",
+    authentication: "plain",
+    user_name: "saracavic",
+    password: "4l3@Fdawgdr00l",
+    enable_starttls_auto: true
+  }
 Depot::Application.initialize!
+end
