75a7627f97fb00f68c4533e0557770382737eb00
Sara Cavic <saracavic@gmail.com>
2014-11-16 21:32:30 -0500

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   protected
     def set_i18n_locale_from_params
       if params[:locale]
-        if I18n.available_locales.map(&:to_s).include?(params[:locale])
-          I18n.locale = params[:locale]
+        if i18n.available_locales.map(&:to_s).include?(params[:locale])
+          i18n.locale = params[:locale]
         else
           flash.now[:notice] =
             "#{params[:locale]} translation not available"
@@ -15,11 +15,9 @@ class ApplicationController < ActionController::Base
         end
       end
     end
-
     def default_url_options
-        { locale: I18n.locale }
+        { locale: i18n.locale }
     end
-
     def authorize
       unless User.find_by(id: session[:user_id])
         redirect_to login_url, notice: "Please log in"
